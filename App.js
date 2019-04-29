02ba6b56ce89101ac04184450458c204218c9f19
Rashila Rathnasiri <rashila.rathnasiri@techsys.lk>
2019-04-29 12:18:45 +0530

    added side drawer

--- App.js
+++ App.js
@@ -1,80 +1,28 @@
-import React, {Component} from 'react';
-import {StyleSheet,View} from 'react-native';
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceList from './src/components/PlaceList/PlaceList';
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-import { connect } from 'react-redux';
-import { addPlace, deletePlace, selectPlace,deselectPlace } from './src/store/actions/index';
+import {Navigation} from "react-native-navigation";
+import { Provider } from 'react-redux';
 
-class App extends Component {
+import AuthScreen from "./src/screens/Auth/Auth";
+import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
+import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
+import PlaceDetailScreen from "./src/screens/PlaceDetail/PlaceDetail";
+import SideDrawer from "./src/screens/SideDrawer/SideDrawer";
+import configureStore from './src/store/configureStore';
 
-placeAddedHandler = placeName => {
-  this.props.onAddPlace(placeName);
-};
+const store = configureStore();
 
-placeDeletedHandler = () => {
-this.props.onDeletePlace();
-};
+//Register Screens
 
-modalClosedHandler = () => {
-  this.props.onDeselectPlace();
-};
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen, store, Provider);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
+Navigation.registerComponent("awesome-places.PlaceDetailScreen", () => PlaceDetailScreen, store, Provider);
+Navigation.registerComponent("awesome-places.SideDrawer", () => SideDrawer);
 
-placeSelectedHandler = key => {
-  this.props.onSelectPlace(key);
-};
-
-  render() {
-    
-    return (
-      <View style={styles.container}>
-
-        <PlaceDetail 
-        selectedPlace={this.props.selectedPlace}
-        onItemDeleted={this.placeDeletedHandler}
-        onModalClosed={this.modalClosedHandler}
-         />
-
-        <PlaceInput 
-        onPlaceAdded={this.placeAddedHandler}
-        />
-
-        <PlaceList 
-        places={this.props.places} 
-        onItemSelected={this.placeSelectedHandler} 
-        />
-
-      </View>
-    );
-  }
-}
- 
-const styles = StyleSheet.create({
-  container: {
-    padding:10,
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center', 
-    backgroundColor: '#F5FCFF',
-  },
-  
+//Start a App
+Navigation.startSingleScreenApp({
+    screen: {
+        screen: "awesome-places.AuthScreen",
+        title: "Login"
+    }
 });
-
-const mapStateToProps = state => {
-  return {
-    places:state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: (name) => dispatch(addPlace(name)),
-    onDeletePlace: () => dispatch(deletePlace()),
-    onSelectPlace: (key) => dispatch(selectPlace(key)),
-    onDeselectPlace: () => dispatch(deselectPlace())
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose  } from 'redux';
 
 import placesReducer from './reducers/places';
 
@@ -6,8 +6,10 @@ const rootReducer = combineReducers({
     places: placesReducer
 });
 
+let composeEnhancers = compose;
+
 const configureStore = () => {
-    return createStore(rootReducer);
+    return createStore(rootReducer, composeEnhancers());
 };
 
 export default configureStore;

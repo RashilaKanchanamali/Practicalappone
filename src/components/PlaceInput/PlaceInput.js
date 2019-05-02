8c32ff872277977550447247eac3b38dc728af31
Rashila Rathnasiri <rashila.rathnasiri@techsys.lk>
2019-05-02 12:39:12 +0530

    up to add animation button

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,60 +1,14 @@
 import React, { Component } from 'react';
 import { View, TextInput, StyleSheet, Button} from 'react-native';
 
-class PlaceInput extends Component {
-    state = {
-        placeName:""
-      };
+import DefaultInput from '../UI/DefaultInput/DefaultInput';
 
-      componentDidMount() {
-        
-      }
-      
-      placeNameChangedHandler = val => {
-        this.setState({
-          placeName: val
-        });
-      };
+const placeInput = props => (
+        <DefaultInput 
+        placeholder="Place Name" 
+        value={props.placeName} 
+        onChangeText={props.onChangeText}
+        />       
+);
 
-      placeSubmitHandler = () => {
-        if (this.state.placeName.trim() === ""){
-          return;
-        }
-      
-        this.props.onPlaceAdded(this.state.placeName);
-      };
-
-    render() {
-        return (
-        <View style={styles.inputContainer}>
-        <TextInput 
-        style = {{width:300, borderColor: "red", borderWidth: 0.5, display: 'flex', borderRadius: 15}}
-        value={this.state.placeName}
-        placeholder={"place name"}
-        onChangeText={this.placeNameChangedHandler}
-        />
-        <Button 
-          style={styles.placeButton}
-          title="Add"
-          onPress={this.placeSubmitHandler}
-        />
-        </View>
-        )
-    }
-
-}
-
-const styles = StyleSheet.create({
-    inputContainer:{
-        //flex:1,
-        flexDirection:"row"
-      },
-      placeInput:{
-        width:"70%"
-      },
-      placeButton:{
-        width:"30%"
-      },
-});
-
-export default PlaceInput;
+export default placeInput;

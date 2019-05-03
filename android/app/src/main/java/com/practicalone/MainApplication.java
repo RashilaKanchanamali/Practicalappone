3e8bf51c5f05c293f7d57891aa7c87beded36439
Rashila Rathnasiri <rashila.rathnasiri@techsys.lk>
2019-05-03 12:14:52 +0530

    befor add maps

--- android/app/src/main/java/com/practicalone/MainApplication.java
+++ android/app/src/main/java/com/practicalone/MainApplication.java
@@ -3,6 +3,7 @@ package com.practicalone;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends NavigationApplication  {
   //   protected List<ReactPackage> getPackages() {
   //     return Arrays.<ReactPackage>asList(
   //         new MainReactPackage(),
+            new MapsPackage(),
   //           new VectorIconsPackage()
   //     );
   //   }
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -8,13 +8,23 @@ import MainText from '../../components/UI/MainText/MainText';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import PickImage from '../../components/PickImage/PickImage';
 import PickLocation from '../../components/PickLocation/PickLocation';
+import validate from '../../utility/validation';
 
 class SharePlaceScreen extends Component {
     static navigatorStyle = {
         navBarButtonColor: "blue"
     }
     state = {
-        placeName: ""
+        controls: {
+            placeName: {
+                value: "",
+                valid: false,
+                touched: false,
+                validationRules: {
+                    notEmpty: true
+                }
+            }
+        }
     };
 
     constructor(props) {
@@ -33,14 +43,24 @@ class SharePlaceScreen extends Component {
     };
 
     placeNameChangedHandler = val => {
-        this.setState({
-            placeName: val
+        this.setState(prevState => {
+            return {
+                controls: {
+                    ...prevState.controls,
+                    placeName: {
+                        ...prevState.controls.placeName,
+                        value: val,
+                        valid: validate(val,prevState.controls.placeName.validationRules),
+                        touched: true
+                    }
+                }
+            }
         });
     }
 
     placeAddedHandler = () => {
-        if (this.state.placeName.trim() !== "") {
-            this.props.onAddPlace(this.state.placeName);
+        if (this.state.controls.placeName.value.trim() !== "") {
+            this.props.onAddPlace(this.state.controls.placeName.value);
         }  
     }
  render () {
@@ -56,12 +76,16 @@ class SharePlaceScreen extends Component {
             <PickLocation/>
 
             <PlaceInput 
-            placeName={this.state.placeName} 
+            placeData={this.state.controls.placeName} 
             onChangeText={this.placeNameChangedHandler}
 
             />
             <View style={styles.button}>
-                <Button title="Share the Place!" onPress={this.placeAddedHandler}/>
+                <Button 
+                    title="Share the Place!" 
+                    onPress={this.placeAddedHandler}
+                    disabled={!this.state.controls.placeName.valid}   
+                    />
             </View>
             {/* <PlaceInput onPlaceAdded= {this.placeAddedHandler} /> */}
             </View>
